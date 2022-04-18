63307eb96bb9c0925b8ef285c0e8feda5e5be39b
Your Name <you@example.com>
2022-04-17 17:28:56 -0700

    Revert "Flail"
    This reverts commit 5d243280e57501689ef579330b561d1c14745d93.

--- tests/test_encassist.py
+++ tests/test_encassist.py
@@ -1,27 +1,5 @@
-import argparse
-
 from saltgang import encassist
 
 
-def parse_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--sku",
-        help="",
-        choices=["macos", "linux", "avid", "universal"],
-        required=True,
-    )
-    parser.add_argument(
-        "--outpath",
-        help="provide the path to where to write the resulting encassist yaml file",
-    )
-
-    args = parser.parse_args(args)
-    print(args)
-    return args
-
-
 def test():
-    # parser = parse_args(sys.argv[1:])
-    args = parse_args(["--sku", "macos"])
-    encassist.main(args)
+    encassist.main(None)
